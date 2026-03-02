a786a7b0cf8917cdbca09091dcc7cd113b1dcf56
Mattia-Giacobelli <giacobelli.mattia12@gmail.com>
2026-03-03 00:34:05 +0100

    Create popular and simulcast pages

--- src/Pages/PopularPage.jsx
+++ src/Pages/PopularPage.jsx
@@ -1,12 +1,48 @@
+import { useAnimes } from "../Contexts/AnimeContext"
+
 export default function PopularPage() {
 
-
+    const { animes } = useAnimes()
 
     return (
 
         <>
 
+            <div className="container">
 
+                <div className="row row-cols-sm-2 row-cols-md-4 row-cols-lg-6">
+
+                    {animes && animes.map(anime => {
+
+                        return (
+
+                            <div key={anime.id} className="col" >
+
+                                <div className="card">
+
+                                    <div className="card-img-top">
+                                        <img src={`${import.meta.env.VITE_LARAVEL_IMG_URL}/${anime.cover}`} alt="cover" />
+                                    </div>
+
+                                    <div className="card-body">
+                                        <h6> {anime.name} </h6>
+
+                                        <span>
+                                            {anime.dubs.length > 0 && anime.subs.length > 0 && 'Dub|Sub'}
+                                            {anime.dubs.length == 0 && anime.subs.length > 0 && 'Sottotitoli'}
+                                            {anime.dubs.length > 0 && anime.subs.length == 0 && 'Doppiaggio'}
+                                        </span>
+                                    </div>
+                                </div>
+
+                            </div>
+                        )
+                    })}
+
+
+                </div>
+
+            </div>
 
         </>
     )
--- src/Pages/SimulcastPage.jsx
+++ src/Pages/SimulcastPage.jsx
@@ -1,12 +1,60 @@
+import { useEffect, useState } from "react"
+import { useAnimes } from "../Contexts/AnimeContext"
+
 export default function SimulcastPage() {
 
+    const { animes } = useAnimes()
+
+    const [simulcast, setSimulcast] = useState()
+
+
+
+    useEffect(() => {
+
+        setSimulcast(animes.filter(anime => anime.dubs?.some(dub => dub.language.toLowerCase() === "italiano") && anime.dubs?.some(dub => dub.language.toLowerCase() === "giapponese")))
+
+    }, [animes])
 
 
     return (
 
         <>
 
+            <div className="container">
 
+                <div className="row row-cols-sm-2 row-cols-md-4 row-cols-lg-6">
+
+                    {simulcast && simulcast.map(anime => {
+
+                        return (
+
+                            <div key={anime.id} className="col" >
+
+                                <div className="card">
+
+                                    <div className="card-img-top">
+                                        <img src={`${import.meta.env.VITE_LARAVEL_IMG_URL}/${anime.cover}`} alt="cover" />
+                                    </div>
+
+                                    <div className="card-body">
+                                        <h6> {anime.name} </h6>
+
+                                        <span>
+                                            {anime.dubs.length > 0 && anime.subs.length > 0 && 'Dub|Sub'}
+                                            {anime.dubs.length == 0 && anime.subs.length > 0 && 'Sottotitoli'}
+                                            {anime.dubs.length > 0 && anime.subs.length == 0 && 'Doppiaggio'}
+                                        </span>
+                                    </div>
+                                </div>
+
+                            </div>
+                        )
+                    })}
+
+
+                </div>
+
+            </div>
 
         </>
     )
